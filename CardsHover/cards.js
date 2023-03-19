1b0d6f8aa5308b8aa00ce98e8f99323a9ea2fc0e
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-19 13:45:26 +0530

    Updated cards

--- CardsHover/cards.js
+++ CardsHover/cards.js
@@ -1,10 +1,13 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      // getBoundingClientRect to get position of mouse relative to top-left corner of card
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+
+    // Set custom card property
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
